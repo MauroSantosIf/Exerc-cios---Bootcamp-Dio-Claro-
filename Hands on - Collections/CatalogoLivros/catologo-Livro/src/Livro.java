7b5f4c3435845b769c26169e5da15ec085178631
Mauro Santos <msp11@aluno.ifal.edu.br>
2025-05-13 14:08:21 -0300

    Add files via upload

---
+++ Hands on - Collections/CatalogoLivros/catologo-Livro/src/Livro.java
@@ -0,0 +1,46 @@
+public class Livro {
+
+    String titulo;
+    String autor;
+    int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro -> " +
+                "Titulo: '" + titulo + '\'' +
+                ", Autor: '" + autor + '\'' +
+                ", Ano de Publicacao: " + anoPublicacao;
+    }
+
+    
+}
